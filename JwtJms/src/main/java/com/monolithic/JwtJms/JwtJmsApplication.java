df84ac7b9b74fa21f25a9c5e682dfa8bdc87c0b4
The_Oldest_Dream <142486738+prasanna-soft-dev@users.noreply.github.com>
2024-12-05 17:08:20 +0530

    Add files via upload

---
+++ JwtJms/src/main/java/com/monolithic/JwtJms/JwtJmsApplication.java
@@ -0,0 +1,13 @@
+package com.monolithic.JwtJms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtJmsApplication.class, args);
+	}
+
+}
---
+++ JwtJms/src/test/java/com/monolithic/JwtJms/JwtJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monolithic.JwtJms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
